484ce72e5f74a0f97b42b7c19782a7e538da2f66
andrewvitrenko <avitrenko2004@gmail.com>
2023-07-31 15:55:23 +0200

    feat(`toolbar`): - developed left side of toolbar - added path alias - developed some logic for menu

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { MenuCategory } from '@/types/toolbar';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
-  title = new FormControl('', [Validators.required]);
+export class ToolbarComponent implements OnInit, OnDestroy {
+  MenuCategory = MenuCategory;
+
+  title = new FormControl('');
+  isMenuOpen = false;
+  menuCategory: MenuCategory | null = null;
 
   save() {
-    if (!this.title.valid) {
-      return;
+    if (!this.title.value) {
+      this.title.setValue('Untitled diagram');
     }
 
     console.log('title', this.title.value);
+  }
+
+  openMenu(event: MouseEvent, category: MenuCategory) {
+    event.stopPropagation();
+    this.isMenuOpen = true;
+    this.menuCategory = category;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+    this.menuCategory = null;
+  }
+
+  ngOnInit() {
+    document.body.addEventListener('click', this.closeMenu.bind(this));
+  }
+
+  ngOnDestroy() {
+    document.body.removeEventListener('click', this.closeMenu.bind(this));
   }
 }
---
+++ src/types/toolbar/index.ts
@@ -0,0 +1,8 @@
+export enum MenuCategory {
+  FILE = 'file',
+  EDIT = 'edit',
+  VIEW = 'view',
+  ARRANGE = 'arrange',
+  EXTRAS = 'extras',
+  HELP = 'help',
+}
